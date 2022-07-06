c3af46350e1c82c33a03bb5295aa8b39973cee71
Asus <yohanawishka2000326@gmail.com>
2022-07-06 11:00:44 +0530

    db

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-    $db_name = 'mysql:host=localhost;dbname=sapu_db';
-    $user_name = 'root';
-    $user_password = '';
+    $db_name = 'mysql:host=localhost;dbname=id19227287_sapu_db';
+    $user_name = 'id19227287_root';
+    $user_password = '!r8Ok3x7kaCs!9K}';
 
     $conn = new PDO($db_name, $user_name, $user_password);
 
