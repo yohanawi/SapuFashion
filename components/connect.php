d0b5b3b6a36be67c5add2e39dad8505cf41e0192
Asus <yohanawishka2000326@gmail.com>
2022-07-06 11:31:54 +0530

    db

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-    $db_name = 'mysql:host=localhost;dbname=id19227287_sapu_db';
-    $user_name = 'id19227287_root';
-    $user_password = '!r8Ok3x7kaCs!9K}';
+    $db_name = 'mysql:host=sql6.freesqldatabase.com;dbname=sql6504335';
+    $user_name = 'sql6504335';
+    $user_password = 'CI4acd4TvS';
 
     $conn = new PDO($db_name, $user_name, $user_password);
 
