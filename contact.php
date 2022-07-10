6fceee37f457bad1a063ad992d6413362472b038
Yohan Awishka <yohanawishka2000326@gmail.com>
2022-07-10 13:03:25 +0530

    map

--- contact.php
+++ contact.php
@@ -88,8 +88,8 @@
             <textarea name="msg" placeholder="your message" cols="30" rows="10" class="box"></textarea>
             <input type="submit" value="send message" name="send" class="btn">
         </form>
-        <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30153.788252261566!2d72.82321484621745!3d19.141690214227783!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b63aceef0c69%3A0x2aa80cf2287dfa3b!2sJogeshwari%20West%2C%20Mumbai%2C%20Maharashtra%20400047!5e0!3m2!1sen!2sin!4v1633431163028!5m2!1sen!2sin" allowfullscreen="" loading="lazy"></iframe>
-    </div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2283.146426885366!2d80.02078967705012!3d7.2247423166488804!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbdb19775896da9ff!2zN8KwMTMnMjkuMSJOIDgwwrAwMScyMi43IkU!5e1!3m2!1sen!2sin!4v1657437780916!5m2!1sen!2sin" width="640" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
 </section>
 
 <!-- contact section ends -->
